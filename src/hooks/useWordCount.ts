72dbfe8ae698c16071cfd77f3b3c00d14ca0665a
Javier Vallinas <ff_valli@hotmail.com>
2021-08-11 09:17:20 +0200

    Add useWordCount hook

---
+++ src/hooks/useWordCount.ts
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { WordCountRegistry, SortingCriteria, WordEntry } from "../types";
+
+const reduceWordsByOccurrence = (
+  registry: WordCountRegistry,
+  word: string
+): WordCountRegistry => {
+  if (!registry[word]) registry[word] = 1;
+  else registry[word]++;
+  return registry;
+};
+
+const getWordCountRegistry = (input?: string): WordCountRegistry => {
+  if (!input) return {};
+  const wordList = input.match(/[\w]*/g);
+
+  return wordList
+    ? wordList.filter((word) => !!word).reduce(reduceWordsByOccurrence, {})
+    : {};
+};
+
+export default function useWordCount(
+  input: string,
+  sortBy: SortingCriteria = SortingCriteria.Alphabetical,
+  isAscOrder: boolean = true
+) {
+  const wordCountRegistry = useMemo(() => getWordCountRegistry(input), [input]);
+  const [wordEntries, setWordEntries] = useState<WordEntry[]>([]);
+
+  const [sortingCriteria, toggleSortingCriteria] =
+    useState<SortingCriteria>(sortBy);
+  const [isAscendingOrder, toggleAscendingOrder] =
+    useState<Boolean>(isAscOrder);
+
+  const sortingFunction = useCallback(
+    (firstItem: WordEntry, secondItem: WordEntry): number => {
+      switch (true) {
+        case firstItem[sortingCriteria] > secondItem[sortingCriteria]:
+          return isAscendingOrder ? 1 : -1;
+        case firstItem[sortingCriteria] < secondItem[sortingCriteria]:
+          return isAscendingOrder ? -1 : 1;
+        default:
+          return 0;
+      }
+    },
+    [sortingCriteria, isAscendingOrder]
+  );
+
+  useEffect(() => {
+    setWordEntries(Object.entries(wordCountRegistry).sort(sortingFunction));
+  }, [wordCountRegistry, sortingFunction]);
+
+  return {
+    wordEntries,
+
+    isAscendingOrder,
+    toggleAscendingOrder,
+
+    sortingCriteria,
+    toggleSortingCriteria,
+  };
+}
